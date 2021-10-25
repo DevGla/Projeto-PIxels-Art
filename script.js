a99f0154399aff8e5afbc79109ea72c8c25b29b3
george lucas <glalmeida.dev@gmail.com>
2021-10-25 20:40:26 -0300

    requisito 7

--- script.js
+++ script.js
@@ -8,5 +8,21 @@ for (let i = 1; i <= 25; i += 1){
 
 window.onload = function () {
     let colorBlack = document.querySelector('#black')
-    colorBlack.className = 'color selected';
+    colorBlack.classList.add('selected');
 }
+
+let clickColor = document.querySelectorAll('.color');
+
+//loop adicionando evento nos elementos
+for (let i = 0; i < clickColor.length; i +=1){
+    clickColor[i].addEventListener('click', getColor)
+};
+
+function getColor (evento){
+    let corSelecionada = document.querySelector('.selected');
+    corSelecionada.classList.remove('selected');
+    evento.target.classList.add('selected');
+}
+
+//pega o elemento selecionado e remove a classe selected
+//adiciona no elemento selecionado com o target
